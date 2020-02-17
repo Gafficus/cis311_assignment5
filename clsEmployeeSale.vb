707bf0b6b97a3b48d24e37a72fb616de19c5d775
Nathan Gaffney <nagaffne@svsu.edu>
2020-02-17 12:32:52 -0500

    asdasda

--- clsEmployeeSale.vb
+++ clsEmployeeSale.vb
@@ -27,4 +27,51 @@ Public Class clsEmployeeSale
     Public sngModelSales As Single
     Public intModelQuantity As Single
     Public sngTotalSales As Single
+    '------------------------------------------------------------
+    '-                Subprogram Name: btnQuit_Click            -
+    '------------------------------------------------------------
+    '-                Written By: Your Name                     -
+    '-                Written On: The date you wrote it         -
+    '------------------------------------------------------------
+    '- Subprogram Purpose:                                      -
+    '-                                                          -
+    '- This subroutine is called whenever the user clicks the   -
+    '- quit button.  The program will prompt if the user really –
+    '- wants to quit, and if the user clicks Yes, the program   -
+    '- will terminate.  If the user click No, the program will  -
+    '- continue to run.                                         –
+    '------------------------------------------------------------
+    '- Parameter Dictionary (in parameter order):               -
+    '- sender – Identifies which particular control raised the  –
+    '-          click event                                     - 
+    '- e – Holds the EventArgs object sent to the routine       -
+    '------------------------------------------------------------
+    '- Local Variable Dictionary (alphabetically):              -
+    '- (None)                                                   -
+    '------------------------------------------------------------
+    Public Sub New(strFirstName As String, strLastName As String, intOrderID As Integer,
+                   intID As Integer, sngGamesSales As Single, intGamesQuantity As Integer,
+                   sngDollsSales As Single, intDollsQuantity As Integer, sngBuildingSales As Single,
+                   intBuildingQuantity As Integer, sngModelSales As Single, intModelQuantity As Single)
+        If strFirstName Is Nothing Then
+            Throw New ArgumentNullException(NameOf(strFirstName))
+        End If
+
+        If strLastName Is Nothing Then
+            Throw New ArgumentNullException(NameOf(strLastName))
+        End If
+
+        Me.strFirstName = strFirstName
+        Me.strLastName = strLastName
+        Me.intOrderID = intOrderID
+        Me.intID = intID
+        Me.sngGamesSales = sngGamesSales
+        Me.intGamesQuantity = intGamesQuantity
+        Me.sngDollsSales = sngDollsSales
+        Me.intDollsQuantity = intDollsQuantity
+        Me.sngBuildingSales = sngBuildingSales
+        Me.intBuildingQuantity = intBuildingQuantity
+        Me.sngModelSales = sngModelSales
+        Me.intModelQuantity = intModelQuantity
+    End Sub
 End Class
--- modMain.vb
+++ modMain.vb
@@ -1,4 +1,23 @@
-﻿Module modMain
+﻿'------------------------------------------------------------
+'-                File Name : Form1.frm                     - 
+'-                Part of Project: AssignX                  -
+'------------------------------------------------------------
+'-                Written By: Your Name                     -
+'-                Written On: The date you wrote it         -
+'------------------------------------------------------------
+'- File Purpose:                                            -
+'- This file contains the main application form where the   -
+'- user will input ...                                      - 
+'------------------------------------------------------------
+'- Program Purpose:                                         -
+'-                                                          -
+'- This program (whatever the program does...)              -
+'------------------------------------------------------------
+'- Global Variable Dictionary (alphabetically):             -
+'- intCounter – Keeps track of the number of items read in. –
+'- sngGrandTotal – Total amount of sales dollars read in.   –
+'------------------------------------------------------------
+Module modMain
 
     Sub Main()
 
